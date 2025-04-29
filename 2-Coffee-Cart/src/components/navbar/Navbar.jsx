e0982744716d20df482ab076f17da4d651616311
Sameer2748 <mrao27488@gmail.com>
2025-04-29 14:39:07 +0530

    new motion project

--- 2-Coffee-Cart/src/components/navbar/Navbar.jsx
+++ 2-Coffee-Cart/src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({onClick}) => {
   return (
     <motion.nav initial={{opacity:0, y:-100}} animate={{opacity:1, y:0}} transition={{type:"spring", stiffness:100, damping:10, delay:1}} className='absolute top-0 left-0 w-full h-[50px] text-white flex items-center justify-between px-10 z-50 mt-7'>
       <div className='w-full h-full bg-gray-800 backdrop-blur-3xl rounded-lg shadow-lg px-4'>
-        <div className='flex items-center justify-between w-full h-full' onClick={onClick}>
+        <div className='flex items-center justify-between w-full h-full' >
             {/* logo section */}
             <h1 className='text-2xl font-semibold text-orange-300  cursor-pointer'>Coders. <span className='text-white'>Cafe</span></h1>
             {/* hamburger section */}
